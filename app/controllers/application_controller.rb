96d73fc05318c7eef7395e6cbbf8652e5c7ff000
Brendan Miranda <me@brendanmiranda.com>
2015-05-30 19:52:42 -0400

    Implement Auth and Sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
-  helper_method :current_user, :require_current_user
+  helper_method :current_user, :logged_in?, :authorize, :require_current_user
 
   def current_user
-    User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by_id(session[:user])
+  end
+
+  def logged_in?
+    current_user != nil
   end
 
   def require_current_user
