92d41df1d4e1d33b20e739bc07cf67515c368fa1
AlexTaber <alex.taber0@gmail.com>
2015-06-02 19:38:48 -0400

    initial pusher implementation

---
+++ app/assets/javascripts/notifications.js
@@ -0,0 +1,33 @@
+$.ajax({
+  url: '/cur_user',
+  method: 'GET'
+}).done(function(response){
+
+
+  var pusher = new Pusher('1115c4a31fc8d8f80c44');
+
+  //subscribe to our notifications channel
+  var notificationsChannel = pusher.subscribe('notifications' + response);
+  console.log(notificationsChannel)
+
+  //do something with our new information
+  notificationsChannel.bind('new_notification', function(notification){
+      // assign the notification's message to a <div></div>
+      var message = notification.message;
+      $('div.notification').text(message);
+  });
+
+  $('.submit-notification').on('click', sendNotification);
+});
+
+var sendNotification = function(){
+  console.log("FIRED")
+
+    // get the contents of the input
+    // var text = $('input.create-notification').val();
+
+    // POST to our server
+    $.post('/notifications/create', {message: "text"}).success(function(){
+        console.log('Notification sent!');
+    });
+};
