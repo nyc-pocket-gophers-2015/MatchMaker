cf365678262a0e7a5b96f61372357f32a319663a
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-06-02 17:44:28 -0400

    adds email templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+   default from: 'notifications@example.com'
+
+  def invite_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: "You've been invited to join MatchMe")
+  end
 end
