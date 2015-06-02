5ac04358d8c2d7a3c4516ad79d303c3e8cdacc71
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-06-02 16:06:53 -0400

    team implemented fb login and site login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = @user.id
-    redirect_to edit_user_path(@user)
+    if params[:provider]
+      @user = User.from_omniauth(env["omniauth.auth"])
+      session[:user_id] = @user.id
+      redirect_to edit_user_path(@user)
+    else
+      user = User.find_by_email(params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to user_path current_user
+      else
+        redirect_to '/login'
+      end
+    end
   end
 
   def destroy
