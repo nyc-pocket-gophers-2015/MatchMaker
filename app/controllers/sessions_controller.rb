6edb8b83d51d7792fa98fc18a906e2b3693e9066
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-06-02 11:43:09 -0400

    sets up facebook login and user creation with facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,12 @@
-# class SessionsController < ApplicationController
-
-# def new
-# end
-
-# def create
-#   user = User.find_by_email(params[:email])
-#     if user && user.authenticate(params[:password])
-#       session[:user_id] = user.id
-#       redirect_to user_path current_user
-#     else
-#       redirect_to '/login'
-#     end
-# end
-
-# def destroy
-#   session[:user_id] = nil
-#   redirect_to '/login'
-# end
-
-
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_url
+    @user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = @user.id
+    redirect_to edit_user_path(@user)
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
+    redirect_to root_path
   end
 end
-
-# end
