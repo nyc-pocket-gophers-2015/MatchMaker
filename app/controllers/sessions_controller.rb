4a70c6682e2cb02d23e11639af8cfae04c6c150a
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-06-02 12:30:05 -0400

    adds sessions for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect_to user_path current_user
-      else
-        redirect_to '/login'
-      end
+    @user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = @user.id
+    redirect_to edit_user_path(@user)
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to root_path
   end
 
 end
