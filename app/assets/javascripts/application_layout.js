28180d7cc92280a9b8511b4019439be85460c803
Brendan Miranda <me@brendanmiranda.com>
2015-06-03 00:30:45 -0400

    Prelim CSS

--- app/assets/javascripts/application_layout.js
+++ app/assets/javascripts/application_layout.js
@@ -3,11 +3,24 @@ $(document).bind("mobileinit", function(){
 });
 
 $(document).ready(function(){
+  $('.ui-content').hide().fadeIn(500);
+  $('.pairing-title').css({ opacity: 1});
+  $('.footer').css({ opacity: 1});
+  $(document.body).delegate("form",'submit',function(e){
+    $(this).attr("data-ajax", "false");
+  });
+  var newHeight = window.innerHeight * 0.8;
+  $('.ui-content').css({'height': newHeight + "px"});
+
   $('.vote-control-no').on('click', function() {
-    $('.vote-form-no').submit();
+    $('.pairing-body').animate({left: "-=600px"}, 500, function(){
+      $('.vote-form-no').submit();
+    });
   });
 
   $('.vote-control-yes').on('click', function() {
-    $('.vote-form-yes').submit();
+    $('.pairing-body').animate({left: "+=600px"}, 500, function(){
+      $('.vote-form-yes').submit();
+    });
   });
 })
