35e8a009ac2e29f2afe961ee98e3765fd1cebf9f
Brendan Miranda <me@brendanmiranda.com>
2015-06-04 15:49:12 -0400

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,7 +72,7 @@ end
 
 User.create(name: "Matchmaker", email: "matchmaker@mm.com", birthday: Faker::Date.between(50.years.ago, 18.years.ago), gender: "male", location: "NYC", password: "123", preferred_gender: pref_gender)
 
-User.create(name: "Tracy Teague", email: "tracy.teague05@gmail.com", birthday: Faker::Date.26.years.ago, gender: "female", location: "NYC", password: "123", preferred_gender: "male")
+User.create(name: "Tracy Teague", email: "tracy.teague05@gmail.com", birthday: Faker::Date.between(26.years.ago, 27.years.ago), gender: "female", location: "NYC", password: "123", preferred_gender: "male")
 
 pairing = Pairing.create(user_id: User.find_by(email: "me@brendanmiranda.com").id, pair_id: User.last.id)
 
